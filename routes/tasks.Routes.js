251e12c63af1f809b3e4fcd1fb04950a0cb4858f
Shubham Kulkarni <skulk156.pc@gmail.com>
2026-01-01 13:47:19 +0530

    s

--- routes/tasks.Routes.js
+++ routes/tasks.Routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { 
   getTasks, 
   getMyTasks, 
-<<<<<<< HEAD
   getTaskById,
   addTask, 
   updateTask, 
@@ -11,19 +10,6 @@ import {
   submitTaskForReview,
   reviewTask,
   getTeamMembers
-=======
-  getTeamMembers, 
-  addTask, 
-  updateTask, 
-  deleteTask, 
-  getTaskById, 
-  deleteAttachment, 
-  updateProgressStatus,
-  // Add the new workflow functions
-  acceptTask,
-  submitTaskForReview,
-  reviewTask
->>>>>>> 69a1868ebc54e916510eeccf928179d6600d558d
 } from "../controllers/taskController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 import multer from "multer";
@@ -45,7 +31,6 @@ const upload = multer({
 // Apply authentication middleware to all routes
 router.use(authMiddleware);
 
-<<<<<<< HEAD
 // Main CRUD and filtering routes
 router.route("/")
   .get(getTasks)
@@ -65,23 +50,4 @@ router.put("/:id/accept", acceptTask);
 router.put("/:id/submit", submitTaskForReview);
 router.put("/:id/review", reviewTask);
 
-=======
-router.get("/", authMiddleware, getTasks);
-router.get("/my-tasks", authMiddleware, getMyTasks);
-router.get("/team-members", authMiddleware, getTeamMembers);
-router.get("/:id", authMiddleware, getTaskById);
-router.post("/", authMiddleware, upload.array('attachments', 5), addTask);
-router.post("/add", authMiddleware, upload.array('attachments', 5), addTask);
-router.put("/:id", authMiddleware, upload.array('attachments', 5), updateTask);
-router.put("/:id/progress", authMiddleware, updateProgressStatus);
-
-// Add the new workflow routes
-router.put("/:id/accept", authMiddleware, acceptTask);
-router.put("/:id/submit", authMiddleware, submitTaskForReview);
-router.put("/:id/review", authMiddleware, reviewTask);
-
-router.delete("/:id", authMiddleware, deleteTask);
-router.delete("/:id/attachments/:attachmentId", authMiddleware, deleteAttachment);
-
->>>>>>> 69a1868ebc54e916510eeccf928179d6600d558d
 export default router;
